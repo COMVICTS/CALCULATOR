53f6edef218ec441f041adb34c9cb251ceb63f46
COMVICTS <COMVICTS@GMAIL.COM>
2022-07-07 15:24:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function calculateExpression(number1,number2,operation){
+    switch(operation){
+
+        case "/":
+            return number1/number2;
+        
+        case "*":
+            return number1*number2;
+
+        case "+":
+            return number1+number2;
+
+        case "-":
+            return number1-number2;
+
+    }
+}
