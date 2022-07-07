132f9b0aca8b75ddad95858fa59a8ed8aba79db8
COMVICTS <COMVICTS@GMAIL.COM>
2022-07-07 18:29:49 +0100

    basic functionality

--- app.js
+++ app.js
@@ -1,17 +1,76 @@
-function calculateExpression(number1,number2,operation){
-    switch(operation){
+const calculatorNumbers = document.querySelector(".displayText");
+const buttons = document.querySelectorAll(".button")
+let num1 = "";
+let num2 = "";
+let oper = "";
+let result = 0;
 
+buttons.forEach(button => {
+    button.addEventListener("click", onButton)
+    
+});
+
+function onButton(){
+
+    if(this.classList.contains("clear")){
+        calculatorNumbers.textContent = "";
+    }
+
+    else if(this.classList.contains("number")){
+        let currentNumber = calculatorNumbers.textContent;
+        let updatedNumber = currentNumber + this.textContent;
+        //updatedNumber is current being displayed
+        calculatorNumbers.textContent = updatedNumber;
+       
+    }
+    else if(this.classList.contains("operator")){
+
+
+        if(this.textContent != "="){
+            console.log
+            let displayedNumber = calculatorNumbers.textContent;
+            let firstNumber = displayedNumber;
+            num1 = displayedNumber;
+            oper = this.textContent;
+
+            calculatorNumbers.textContent = "";
+            
+        }
+
+        else{
+            num2 = calculatorNumbers.textContent
+            calculations(num1,num2,oper);
+        } 
+        }
+    }
+
+
+function calculations(first,second,sign){
+    first = parseInt(first);
+    second = parseInt(second);
+
+    switch(sign){
         case "/":
-            return number1/number2;
+
+        result = first/second;
+            break;
         
         case "*":
-            return number1*number2;
+
+            result = first*second;
+            break;
 
         case "+":
-            return number1+number2;
+            result = first+second;
+            break;
 
         case "-":
-            return number1-number2;
+            result = first-second;
+            break;
 
     }
+
+    calculatorNumbers.textContent = result;
+    
 }
+
